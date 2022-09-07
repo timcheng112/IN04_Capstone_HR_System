6912d9135acc3b86d8f3008deb31c760919a0352
Timothy <timcheng112@gmail.com>
2022-09-08 01:42:52 +0800

    first commit

---
+++ Spring_Boot/hrsystem/src/main/java/com/conceiversolutions/hrsystem/HrsystemApplication.java
@@ -0,0 +1,13 @@
+package com.conceiversolutions.hrsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrsystemApplication.class, args);
+	}
+
+}
---
+++ Spring_Boot/hrsystem/src/test/java/com/conceiversolutions/hrsystem/HrsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conceiversolutions.hrsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
