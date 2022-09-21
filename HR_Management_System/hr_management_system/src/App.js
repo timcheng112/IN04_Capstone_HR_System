b0ea0e15d7ad4f892c9eda5bd0c055066c0bcbb3
Timothy <timcheng112@gmail.com>
2022-09-21 12:26:24 +0800

    Added Entities for Springboot and Added preliminary onboarding frontend

--- HR_Management_System/hr_management_system/src/App.js
+++ HR_Management_System/hr_management_system/src/App.js
@@ -1,12 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
+import OnboardingHr from "./pages/OnboardingHr";
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello HR Management System!
-    </h1>
-  );
+  return <OnboardingHr />;
 }
 
 export default App;
