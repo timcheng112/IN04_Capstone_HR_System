1f7f49e35da84467ba0fa70df5cd54d338ec8d65
SH dubU <wshihan@u.nus.edu>
2022-09-30 10:44:44 +0800

    trying to do orgchart, pushing for alison

--- HR_Management_System/hr_management_system/src/pages/OrgChart/ViewOrgChart/viewOrgChart.js
+++ HR_Management_System/hr_management_system/src/pages/OrgChart/ViewOrgChart/viewOrgChart.js
@@ -1,27 +1,48 @@
-import * as React from "react";
+import React, { Fragment } from "react";
+import randomcolor from "randomcolor";
 import data from "./data.json";
-import "./orgChart.css"
-
+function randomIntFromInterval(min, max) { // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 const Card = (props) => {
+  const levelColor = randomcolor();
+
   return (
-    <>
-      {props.data.map((item) => (
-        <>
-          <li className="card">
-            {item.name}
+    <ul>
+      {props.data.map((item,index) => (
+        <Fragment key={item.name}>
+          <li>
+            <div className="card">
+              <div className="image">
+                <img
+                  src={"https://randomuser.me/api/portraits/men/"+randomIntFromInterval(1,100)+".jpg"}
+                  alt="Profile"
+                  style={{ borderColor: levelColor }}
+                />
+              </div>
+              <div className="card-body">
+                <h4>{item.name}</h4>
+                <p>{item.jobtitle}</p>
+              </div>
+              <div className="card-footer" style={{ background: levelColor }}>
+              temp
+              </div>
+              <div></div>
+            </div>
             {item.children?.length && <Card data={item.children} />}
           </li>
-        </>
+        </Fragment>
       ))}
-    </>
+    </ul>
   );
 };
-const EmployeeChart = (props) => {
+
+const EmployeeChart = () => {
   return (
     <div className="org-tree">
-      Employee Chart
       <Card data={data} />
     </div>
   );
 };
+
 export default EmployeeChart;
