9a1f73b6b3ac5ce3f54998a972fc1e7180ae58c1
plzdonthurtme <bless3bluemoon.al@gmail.com>
2022-09-30 10:13:18 +0800

    dept arraylist + routing wip + updateProfile fe + view team axois

--- HR_Management_System/hr_management_system/src/App.js
+++ HR_Management_System/hr_management_system/src/App.js
@@ -13,25 +13,30 @@ import OnboardingHr from "./pages/OnboardingHr";
 import ViewTeam from "./pages/OrgChart/ViewTeam/viewTeam";
 import ViewDepartment from "./pages/OrgChart/ViewDepartment/viewDepartment";
 import ViewOrganisation from "./pages/OrgChart/ViewOrganisation/viewOrganisation";
+import ProfilePage from './pages/ProfilePage';
+import UpdateProfile from './pages/UpdateProfile';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <PublicRoute exact path="/" component={LoginPage} />
-          <PublicRoute exact path="/register" component={RegisterPage} />
-          <PublicRoute exact path="/forgot" component={ForgotPasswordPage} />
-          <PublicRoute exact path="/verify" component={VerificationPage} />
-          <PublicRoute exact path="/verify/:token" component={VerificationPage} />
-          <PublicRoute exact path="/test" component={Timeout} />
-          <PrivateRoute exact path="/onboarding" component={OnboardingHrPage} />
-          
-        </Switch>
-      </div>
-    </BrowserRouter>
+//    <BrowserRouter>
+//      <div className="App">
+//        <Switch>
+//          <PublicRoute exact path="/" component={LoginPage} />
+//          <PublicRoute exact path="/register" component={RegisterPage} />
+//          <PublicRoute exact path="/forgot" component={ForgotPasswordPage} />
+//          <PublicRoute exact path="/verify" component={VerificationPage} />
+//          <PublicRoute exact path="/verify/:token" component={VerificationPage} />
+//          <PublicRoute exact path="/test" component={Timeout} />
+//          <PrivateRoute exact path="/onboarding" component={OnboardingHrPage} />
+//          <PublicRoute exact path="/profile" component={ProfilePage}/>
+//          <PublicRoute exact path="/viewOrg" component={ViewOrganisation}/>
+//          <PublicRoute exact path="/updateProfile" component={UpdateProfile}/>
+//
+//        </Switch>
+//      </div>
+//    </BrowserRouter>
 //    <OnboardingHr />
-//    <ViewTeam/>
+    <ViewTeam/>
 //    <ViewDepartment />
  //   <ViewOrganisation />
 
