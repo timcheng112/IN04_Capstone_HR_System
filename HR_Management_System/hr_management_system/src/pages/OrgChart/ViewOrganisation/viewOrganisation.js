a22ee8dff208b300f359c9ce36c63c4d64c2b426
xueqi <72065687+dengxueqi@users.noreply.github.com>
2022-10-03 04:26:46 +0800

    routing for view department, view team. viewDepartment - display data in table

--- HR_Management_System/hr_management_system/src/pages/OrgChart/ViewOrganisation/viewOrganisation.js
+++ HR_Management_System/hr_management_system/src/pages/OrgChart/ViewOrganisation/viewOrganisation.js
@@ -35,6 +35,7 @@ export default function ViewOrganisation() {
 
   const [openChange, setOpenChange] = useState(false);
   const [deptName, setDeptName] = useState("");
+  const history = useHistory()
 
 
   return (
@@ -49,7 +50,8 @@ export default function ViewOrganisation() {
 
           <DeleteDeptModal
             open={openDelete}
-            onClose={() => setOpenDelete(false)}
+            onClose={() => setOpenDelete(false)} 
+            />
 
           <ChangeOrgHeadModal
             newOrgName={org.organizationName}
@@ -233,7 +235,8 @@ export default function ViewOrganisation() {
                               </td>
                               <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                                 {/*THE VIEW BUTTON IS HERE!!!!*/}
-                                <button className="text-indigo-600 hover:text-indigo-900">
+                                <button className="text-indigo-600 hover:text-indigo-900"
+                                onClick={() => history.push("/viewDept/" + dept.departmentId)}>
                                   View
                                   <span className="sr-only">, {dept.name}</span>
                                 </button>
