c851e4fc1cab021fccecc415b672012bc41fcd8a
KongXinyue <e0376969@u.nus.edu>
2022-10-03 08:07:11 +0800

    delete task

--- HR_Management_System/hr_management_system/src/pages/Onboarding/TaskOption.js
+++ HR_Management_System/hr_management_system/src/pages/Onboarding/TaskOption.js
@@ -86,6 +86,7 @@ export default function TaskOptions({ task, refreshKeyHandler}) {
       <button
         type="button"
         className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        onClick={() => setOpenDelete(true)}
       >
         <TrashIcon className="md:-ml-0.5 md:mr-2 h-4 w-4" aria-hidden="true" />
         <span className="hidden md:block">Delete</span>
