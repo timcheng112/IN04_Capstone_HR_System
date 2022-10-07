da06e8fe284deffae4b1b176788e626779e71177
Matthew <e0556151@u.nus.edu>
2022-10-07 08:50:28 +0800

    Bugfix for Leave table and JobRequestRepository

--- Spring_Boot/hrsystem/src/main/java/com/conceiversolutions/hrsystem/jobmanagement/jobrequest/JobRequestRepository.java
+++ Spring_Boot/hrsystem/src/main/java/com/conceiversolutions/hrsystem/jobmanagement/jobrequest/JobRequestRepository.java
@@ -10,15 +10,15 @@ import java.util.List;
 @Repository
 public interface JobRequestRepository extends JpaRepository<JobRequest, Long> {
     @Query("SELECT jr FROM JobRequest jr WHERE jr.jobTitle LIKE %:title%")
-    List<JobRequest> findJobRequestsByIncludeTitle(@Param("title") String title);
+    List<JobRequest> findJobRequestsByIncludeTitle(String title);
 
     @Query("SELECT jr FROM JobRequest jr WHERE jr.jobDescription LIKE %:description%")
-    List<JobRequest> findJobRequestsByIncludeDescription(@Param("description") String description);
+    List<JobRequest> findJobRequestsByIncludeDescription(String description);
 
     @Query("SELECT jr FROM JobRequest jr WHERE jr.department.departmentId = ?1")
     List<JobRequest> findJobRequestsByDepartmentId(Long departmentId);
 
-    @Query("SELECT jr FROM JobRequest jr WHERE jr.requestor.userId = ?1")
+    @Query("SELECT jr FROM JobRequest jr WHERE jr.requestedBy.userId = ?1")
     List<JobRequest> findJobRequestsByRequestorId(Long requestorId);
 
     @Query("SELECT jr FROM JobRequest jr WHERE jr.approver.userId = ?1")
