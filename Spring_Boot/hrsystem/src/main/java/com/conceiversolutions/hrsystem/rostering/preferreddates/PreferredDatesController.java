91f9a2ebe59416812ddbcd9e7dccb332774d0d6e
Timothy <timcheng112@gmail.com>
2022-10-09 19:36:26 +0800

    Added CRD for PreferredDates Entity

--- Spring_Boot/hrsystem/src/main/java/com/conceiversolutions/hrsystem/rostering/preferreddates/PreferredDatesController.java
+++ Spring_Boot/hrsystem/src/main/java/com/conceiversolutions/hrsystem/rostering/preferreddates/PreferredDatesController.java
@@ -1,7 +1,15 @@
 package com.conceiversolutions.hrsystem.rostering.preferreddates;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -13,4 +21,24 @@ public class PreferredDatesController {
 
     private final PreferredDatesService preferredDatesService;
 
+    @GetMapping
+    public List<PreferredDates> getPreferredDates() {
+        return preferredDatesService.getPreferredDates();
+    }
+
+    @GetMapping(path = "{preferredDatesId}")
+    public PreferredDates getPreferredDatesById(@PathVariable("preferredDatesId") Long preferredDatesId) {
+        return preferredDatesService.getPreferredDatesById(preferredDatesId);
+    }
+
+    @PostMapping
+    public Long addNewPreferredDates(@RequestBody PreferredDates preferredDates,
+            @RequestParam(name = "userId", required = true) Long userId) {
+        return preferredDatesService.addNewPreferredDates(preferredDates, userId);
+    }
+
+    @DeleteMapping(path = "{preferredDatesId}")
+    public void deletePreferredDates(@PathVariable("preferredDatesId") Long preferredDatesId) {
+        preferredDatesService.deletePreferredDates(preferredDatesId);
+    }
 }
--- Spring_Boot/hrsystem/src/main/java/com/conceiversolutions/hrsystem/rostering/preferreddates/PreferredDatesService.java
+++ Spring_Boot/hrsystem/src/main/java/com/conceiversolutions/hrsystem/rostering/preferreddates/PreferredDatesService.java
@@ -1,6 +1,11 @@
 package com.conceiversolutions.hrsystem.rostering.preferreddates;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.conceiversolutions.hrsystem.user.user.User;
+import com.conceiversolutions.hrsystem.user.user.UserRepository;
 
 import lombok.AllArgsConstructor;
 
@@ -9,5 +14,44 @@ import lombok.AllArgsConstructor;
 public class PreferredDatesService {
 
     private final PreferredDatesRepository preferredDatesRepository;
+    private final UserRepository userRepository;
+
+    public List<PreferredDates> getPreferredDates() {
+        List<PreferredDates> dates = preferredDatesRepository.findAll();
+        for (PreferredDates date : dates) {
+            date.setUser(null);
+        }
+        return dates;
+    }
+
+    public PreferredDates getPreferredDatesById(Long preferredDatesId) {
+        PreferredDates dates = preferredDatesRepository.findById(preferredDatesId).orElseThrow(
+                () -> new IllegalStateException("Preferred Dates with ID: " + preferredDatesId + " does not exist!"));
+        dates.setUser(null);
+        return dates;
+    }
+
+    public Long addNewPreferredDates(PreferredDates preferredDates, Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalStateException("User with ID: " + userId + " does not exist!"));
+
+        preferredDates.setUser(user);
+        PreferredDates savedDates = preferredDatesRepository.saveAndFlush(preferredDates);
+
+        user.setPreferredDates(preferredDates);
+        userRepository.saveAndFlush(user);
+
+        return savedDates.getPreferredDatesId();
+    }
+
+    public void deletePreferredDates(Long preferredDatesId) {
+        PreferredDates dates = preferredDatesRepository.findById(preferredDatesId).orElseThrow(
+                () -> new IllegalStateException("Preferred Dates with ID: " + preferredDatesId + " does not exist!"));
+
+        dates.getUser().setPreferredDates(null);
+        dates.setUser(null);
+
+        preferredDatesRepository.deleteById(preferredDatesId);
+    }
 
 }
