c42626e2485b162f4334f7f621eda94b66f89cc5
SH dubU <wshihan@u.nus.edu>
2022-10-13 14:34:23 +0800

    Created Info Panel for rostering

--- HR_Management_System/hr_management_system/src/pages/Rostering/roster.js
+++ HR_Management_System/hr_management_system/src/pages/Rostering/roster.js
@@ -4,6 +4,7 @@ import api from "../../utils/api.js";
 import AddShiftModal from "../../features/rostering/AddShiftModal.js";
 import ViewTemplateShiftsSlideover from "../../features/rostering/ViewTemplateShiftsSlideover.js";
 import ComboBox from "../../components/ComboBox/ComboBox.js";
+import InfoPanel from "../../components/rostering/InfoPanel.js";
 
 const people = [
   {
@@ -107,6 +108,8 @@ export default function Roster() {
         <div className="mt-8 flex flex-col">
           <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              {/* think add the component here? not sure... */}
+              <InfoPanel />
               <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
                 <table className="min-w-full divide-y divide-gray-300">
                   <thead className="bg-gray-50">
