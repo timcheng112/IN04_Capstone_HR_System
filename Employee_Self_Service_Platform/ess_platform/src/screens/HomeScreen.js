ac92555dc3926a76513c5f26f6ace03ce16a9e2b
plzdonthurtme <81510156+plzdonthurtme@users.noreply.github.com>
2022-10-13 23:45:40 +0800

    changes for training

--- Employee_Self_Service_Platform/ess_platform/src/screens/HomeScreen.js
+++ Employee_Self_Service_Platform/ess_platform/src/screens/HomeScreen.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Text } from "react-native-paper";
+// import PlaylistPage from "../components/training/PlaylistPage";
+import TrainingHR from "../components/training/TrainingHR"; 
+// import PlaylistPage from "../components/training/PlaylistPage";
 
 const HomeScreen = () => {
   return <Text>HomeScreen</Text>;
+
 };
 
 export default HomeScreen;
