c16d5e2d46f1bd12381251c5db86165072e700a4
plzdonthurtme <81510156+plzdonthurtme@users.noreply.github.com>
2022-10-19 07:36:20 +0800

    deleteAllNotif done

--- HR_Management_System/hr_management_system/src/pages/NotificationPage/AllNotificationPage.js
+++ HR_Management_System/hr_management_system/src/pages/NotificationPage/AllNotificationPage.js
@@ -1,12 +1,12 @@
-import { EnvelopeIcon, TrashIcon } from '@heroicons/react/20/solid'
-import Navbar from '../../components/Navbar';
-import { useLayoutEffect, useRef, useState, useEffect } from 'react'
+import { EnvelopeIcon, TrashIcon } from "@heroicons/react/20/solid";
+import Navbar from "../../components/Navbar";
+import { useLayoutEffect, useRef, useState, useEffect } from "react";
 
-import api from '../../utils/api'
+import api from "../../utils/api";
 import { deleteUser, getUserId } from "../../utils/Common.js";
 // import { getUserId } from "../../Common";
 import { useHistory } from "react-router";
-import Notification from '../../components/Notification';
+import Notification from "../../components/Notification";
 /*
 
 
@@ -23,8 +23,6 @@ import Notification from '../../components/Notification';
   }
   ```
 */
-
-
 
 // const messages = [
 //   {
@@ -48,121 +46,110 @@ import Notification from '../../components/Notification';
 //   // More messages...
 // ];
 
-
 export default function Example() {
+  // const [userId, setUser] = useState("");
+  // setUser(getUserId());
+  const userId = getUserId();
+  //   console.log(userId);
+  const history = useHistory();
 
-    // const [userId, setUser] = useState("");
-    // setUser(getUserId());
-    const userId = getUserId();
-    console.log(userId);
+  const [notification, setNotification] = useState([]);
 
-    const [notification, setNotification] = useState([]);
-    let [userInfo, setUserInfo] = useState([]);
-    
-    // useEffect(() => {
-    //     api
-    //       .getAllNotifications().then((response) => {
-    //         console.log(response.data);
-    //         setNotification(response.data);
-    
-    //       })
-    //       .catch((error) => {
-    //         console.log(error);
-    //         alert("Unable to get all employees right now")
-    //       });
-    //   }, []);
+  // useEffect(() => {
+  //     api
+  //       .getAllNotifications().then((response) => {
+  //         console.log(response.data);
+  //         setNotification(response.data);
 
-      useEffect(() => {
-        // console.log(userId)
-        api
-          .getAllNotificationsForUser(getUserId()).then((response) => {
-            // console.log(response.data);
-            setNotification(response.data);
-    
-          })
-          .catch((error) => {
-            console.log(error);
-            alert("Unable to get all notifications right now")
-          });
-      },[]);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //         alert("Unable to get all employees right now")
+  //       });
+  //   }, []);
 
-      useEffect(() => {
-            async function getUserInfo() {
-              await api.getUserInfo(userId).then((response) => {
-                setUserInfo(response.data);
-                // console.log(userInfo);
-              });
-            }
-            getUserInfo();
-          }, [userId, userInfo]);
+  useEffect(() => {
+    // console.log(userId)
+    api
+      .getAllNotificationsForUser(getUserId())
+      .then((response) => {
+        // console.log(response.data);
+        setNotification(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Unable to get all notifications right now");
+      });
+  }, []);
 
-        
-        // const deleteANotification = ({notificationId}) => {
-        // // evt.preventDefault();
-        // // console.log(userInfo.authorities);
-        // const boo = window.confirm("Are you sure you want to delete this notification?");
-        
-        // if(boo){
-        //     api
-        //     .deleteANotification(notificationId, getUserId())
-        //     .then((response) => {
-        //         console.log(response.data);
-        //         setNotification(response.data);
-                
-        //     })
-        //     .then(() => {
-        //         console.log("are you here");
+  const deleteANotification = ( notificationId ) => {
+    // evt.preventDefault();
+    // console.log(userInfo.authorities);
+    const boo = window.confirm(
+      "Are you sure you want to delete this notification?"
+    );
 
-        //         // history.push("/home");
-        //     }).catch((error) => {
-        //         console.log(error);
-        //         alert("Unable to delete your notification right now")
-        //     });
-        // }
-        // };
-          
-        // const deleteAllNotifications = ({notificationId}) => {
-        //     // evt.preventDefault();
-        //     // console.log(userInfo.authorities);
-        //     const boo = window.confirm("Are you sure you want to delete this notification?");
-            
-        //     if(boo){
-        //         api
-        //         .deleteAllNotifications(getUserId()).then((response) => {
-        //             console.log(response.data);
-        //             setNotification(response.data);
-            
-        //             })
-        //         .then(() => {
-        //             console.log("are you here");
-    
-        //             // history.push("/home");
-        //         }).catch((error) => {
-        //             console.log(error);
-        //             alert("Unable to delete your notification right now")
-        //         });
-        //     }
-        //     };
+    if(boo){
+      api
+        .deleteANotification(notificationId, getUserId())
+        .then((response) => {
+          console.log(response.data);
+          //   setNotification(response.data);
+        })
+        .then(() => {
+          console.log("are you here");
+          // history.push("/home");
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Unable to delete your notification right now");
+        });
+    }
+  };
 
-        // useEffect(() => {
-        //     // console.log(userId)
-        //     api
-        //         .deleteAllNotifications(getUserId()).then((response) => {
-        //         // console.log(response.data);
-        //         setNotification(response.data);
-        
-        //         })
-        //         .catch((error) => {
-        //         console.log(error);
-        //         alert("Unable to delete all your notifications right now")
-        //         });
-        //     },[]);
+  const deleteAllNotifications = (notificationId) => {
+    // evt.preventDefault();
+    // console.log(userInfo.authorities);
+    const boo = window.confirm(
+      "Are you sure you want to delete all notifications?"
+    );
 
-        
+    if (boo) {
+      api
+        .deleteAllNotifications(getUserId())
+        .then((response) => {
+          console.log(response.data);
+          //   setNotification(response.data);
+        })
+        .then(() => {
+          console.log("are you here");
+
+          history.push("/home");
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Unable to delete your notification right now");
+        });
+    }
+  };
+
+  // useEffect(() => {
+  //     // console.log(userId)
+  //     api
+  //         .deleteAllNotifications(getUserId()).then((response) => {
+  //         // console.log(response.data);
+  //         setNotification(response.data);
+
+  //         })
+  //         .catch((error) => {
+  //         console.log(error);
+  //         alert("Unable to delete all your notifications right now")
+  //         });
+  //     },[]);
+
   return (
-
     <>
-    <Navbar/>
+      <Navbar />
       <div className="relative bg-indigo-800">
         <div className="absolute inset-0">
           <img
@@ -179,28 +166,22 @@ export default function Example() {
           <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl lg:text-6xl">
             All Notifications
           </h1>
-
         </div>
       </div>
 
-        
-
-
-
       {/* start of list */}
       <ul role="list" className="divide-y divide-gray-200 p-40">
-            
-            
-            {/* trash button to trash all */}
-            <button
-                type="button"
-                className="inline-flex items-left rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 m-4"
-                // onClick={()=> {deleteAllNotifications()}}
-            >
-                <TrashIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
-                Delete All Notifications
-            </button>
-
+        {/* trash button to trash all */}
+        <button
+          type="button"
+          className="inline-flex items-left rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 m-4"
+          onClick={() => {
+            deleteAllNotifications();
+          }}
+        >
+          <TrashIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+          Delete All Notifications
+        </button>
 
         {notification.map((message) => (
           <li
@@ -208,18 +189,24 @@ export default function Example() {
             className="relative bg-white py-5 px-4 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 hover:bg-gray-50"
           >
             <div className="flex justify-between space-x-3">
-              
-            <button
+              <button
                 type="button"
                 className="inline-flex items-left rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 m-4"
-                // onClick={() => {deleteANotification(message.id)}}
-            >
-                <TrashIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+                onClick={() => {
+                  deleteANotification(message.id);
+                }}
+              >
+                <TrashIcon
+                  className="-ml-0.5 mr-2 h-4 w-4"
+                  aria-hidden="true"
+                />
                 Delete
-            </button>
+              </button>
               <div className="min-w-0 flex-1">
-              
-                <a href="/NotificationExpandPage" className="block focus:outline-none">
+                <a
+                  href="/NotificationExpandPage"
+                  className="block focus:outline-none"
+                >
                   <span className="absolute inset-0" aria-hidden="true" />
 
                   <p className="truncate text-sm text-black-1500 font-bold">
@@ -236,7 +223,7 @@ export default function Example() {
             </div>
             <div className="mt-1">
               <p className="text-sm text-gray-600 line-clamp-1">
-                {message.description}   
+                {message.description}
               </p>
             </div>
           </li>
