2e08ac42cbadac3327caaed4455620cfc77d3f5a
Timothy <timcheng112@gmail.com>
2022-10-19 09:26:44 +0800

    Updated modals

--- HR_Management_System/hr_management_system/src/features/rostering/AddShiftModal.js
+++ HR_Management_System/hr_management_system/src/features/rostering/AddShiftModal.js
@@ -142,7 +142,12 @@ export default function AddShiftModal({
 
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={onClose}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        onClose={onClose}
+        // disableScrollLock
+      >
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -155,7 +160,8 @@ export default function AddShiftModal({
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
-        <div className="fixed inset-0 z-10 overflow-y-auto">
+        {/* <div className="fixed inset-0 z-10 overflow-y-auto"> */}
+        <div className="fixed inset-0 z-10">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
@@ -166,7 +172,7 @@ export default function AddShiftModal({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+              <Dialog.Panel className="relative transform h-[600px] overflow-scroll rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
                 <div className="mt-3 sm:mt-5">
                   <Dialog.Title
                     as="h3"
