22fe1cee33c04f050a689e08efc585f1215e690b
Matthew <79011015+lee97matthew@users.noreply.github.com>
2022-11-09 06:58:46 +0800

    minor change

--- Job_Management_Platform/job_management_platform/src/pages/JobPosting/jobDetail.js
+++ Job_Management_Platform/job_management_platform/src/pages/JobPosting/jobDetail.js
@@ -114,7 +114,7 @@ export default function JobDetail() {
                 </div>
                 <div className="mt-2 flex items-center text-sm text-gray-500">
                   <CurrencyDollarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                  ${salaryMin}k &ndash; ${salaryMax}k
+                  ${salaryMin} &ndash; ${salaryMax}
                 </div>
                 <div className="mt-2 flex items-center text-sm text-gray-500">
                   <CalendarIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
