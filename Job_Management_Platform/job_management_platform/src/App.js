2a9399c7987f22948716d36d5ceccf78f42337a0
Matthew <79011015+lee97matthew@users.noreply.github.com>
2022-11-09 02:15:55 +0800

    JMP Frontend Job Offer Page - Created jobOffer.js page and added to App.js PrivateRoute

--- Job_Management_Platform/job_management_platform/src/App.js
+++ Job_Management_Platform/job_management_platform/src/App.js
@@ -15,6 +15,7 @@ import JobPostingPage from "./pages/JobPosting";
 import JobApplicationPage from "./pages/JobPosting/apply";
 import FavouriteJobPage from "./pages/JobPosting/favourite";
 import JobDetailPage from "./pages/JobPosting/jobDetail";
+import JobOfferPage from "./pages/JobPosting/jobOffer";
 import Home from "./pages/Home";
 import AllNotificationPage from "./pages/NotificationPage/AllNotificationPage";
 import AddNotification from "./pages/NotificationPage/AddNotification";
@@ -43,6 +44,7 @@ function App() {
           <PrivateRoute exact path="/myapplication" component={JobApplicationPage} />
           <PrivateRoute exact path="/myfavourite" component={FavouriteJobPage} />
           <PrivateRoute exact path="/jobdetail" component={JobDetailPage} />
+          <PrivateRoute exact path="/myoffer" component={JobDetailPage} />
 
           <PrivateRoute exact path="/AllNotifications" component={AllNotificationPage}/>
           <PrivateRoute exact path="/AddNotification" component={AddNotification}/>
