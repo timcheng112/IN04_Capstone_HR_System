273fa2e9a285bd07a0175286fb9024568ffd2091
KongXinyue <e0376969@u.nus.edu>
2022-11-08 12:01:02 +0800

    fix error

--- HR_Management_System/hr_management_system/src/App.js
+++ HR_Management_System/hr_management_system/src/App.js
@@ -32,9 +32,6 @@ import NewJobRequestPage from "./pages/JobRequest/NewRequest";
 import JobRequestHrPage from "./pages/JobRequest/indexHR";
 import JobPostPage from "./pages/JobPost/indexHR";
 import AllApplicantsPage from "./pages/JobPost/JobApplicants";
-import InterviewApplicantsPage from "./pages/JobPost/InterviewApplicants";
-import OfferApplicantsPage from "./pages/JobPost/OfferApplicants";
-import RejectApplicantsPage from "./pages/JobPost/RejectApplicants";
 import ApplicantDetailPage from "./pages/JobPost/ApplicantDetail";
 
 import VideoPage from "./pages/Video";
