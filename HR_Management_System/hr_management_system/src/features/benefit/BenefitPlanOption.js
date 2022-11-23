50fb41a0552c741e78265efa76f06cc2f497e39e
KongXinyue <e0376969@u.nus.edu>
2022-11-23 19:20:16 +0800

    add new claim frontend

--- HR_Management_System/hr_management_system/src/features/benefit/BenefitPlanOption.js
+++ HR_Management_System/hr_management_system/src/features/benefit/BenefitPlanOption.js
@@ -7,12 +7,14 @@ import { useState, useEffect } from "react";
 import api from "../../utils/api";
 import { getUserId } from "../../utils/Common";
 import ViewPlan from "./ViewPlan";
+import AddNewClaim from "./AddNewClaim";
 import CheckDialog from "./CheckDialog";
 import { useHistory } from 'react-router-dom';
 
 export default function BenefitPlanOption({ plan }) {
   const history = useHistory();
   const [open, setOpen] = useState(false);
+  const [claim, setClaim] = useState(false);
   const [trash, setTrash] = useState(false);
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
@@ -50,7 +52,7 @@ export default function BenefitPlanOption({ plan }) {
         <button
           type="button"
           className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          onClick={() => setTrash(true)}
+          onClick={() => setClaim(true)}
         >
           <PlusIcon
             className="md:-ml-0.5 md:mr-2 h-4 w-4"
@@ -70,6 +72,7 @@ export default function BenefitPlanOption({ plan }) {
           <span className="hidden md:block">Terminate</span>
         </button>}
       </div>
+      <AddNewClaim open={claim} setOpen={setClaim} plan={plan}/>
       <ViewPlan open={open} setOpen={setOpen} plan={plan}/>
       <CheckDialog 
           title="Plan"
