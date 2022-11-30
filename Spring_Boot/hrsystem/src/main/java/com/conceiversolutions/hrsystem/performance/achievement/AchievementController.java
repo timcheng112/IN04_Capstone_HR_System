aae72ba8f4b8fc659718eb88035b852acb9a01c3
xueqi <72065687+dengxueqi@users.noreply.github.com>
2022-11-30 15:28:06 +0800

    achievement update and delete backend

--- Spring_Boot/hrsystem/src/main/java/com/conceiversolutions/hrsystem/performance/achievement/AchievementController.java
+++ Spring_Boot/hrsystem/src/main/java/com/conceiversolutions/hrsystem/performance/achievement/AchievementController.java
@@ -1,7 +1,11 @@
 package com.conceiversolutions.hrsystem.performance.achievement;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("*")
@@ -15,8 +19,14 @@ public class AchievementController {
         this.achievementService = achievementService;
     }
 
-    public String deleteAchievement(Long achievementId){
+    @DeleteMapping(path = "{achievementId}")
+    public String deleteAchievement(@PathVariable("achievementId") Long achievementId){
         return achievementService.deleteAchievement(achievementId);
+    }
+
+    @PutMapping(path = "{achievementId}")
+    public String editAchievement(@PathVariable("achievementId") Long achievementId, @RequestParam("description") String description) {
+        return achievementService.editAchievement(achievementId, description);
     }
 
 //    public void editAchievement(Long achievementId){
--- Spring_Boot/hrsystem/src/main/java/com/conceiversolutions/hrsystem/performance/achievement/AchievementService.java
+++ Spring_Boot/hrsystem/src/main/java/com/conceiversolutions/hrsystem/performance/achievement/AchievementService.java
@@ -1,6 +1,11 @@
 package com.conceiversolutions.hrsystem.performance.achievement;
 
 import lombok.AllArgsConstructor;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +21,24 @@ public class AchievementService {
     }
 
     public String deleteAchievement(Long achievementId){
-        System.out.println("achieve.delete");
+        
         achievementRepository.deleteById(achievementId);
-        return "achievement with ID: " + achievementId + "is successfully deleted.";
+        return "Achievement has been successfully deleted";
 
+    }
+
+    @Transactional
+    public String editAchievement(Long achievementId, String description) {
+        Optional<Achievement> optionalA = achievementRepository.findById(achievementId);
+        
+        if (optionalA.isPresent()) {
+            Achievement a = optionalA.get();
+            a.setDescription(description);
+            return "Achievement has been successfully edited";
+        } else {
+            throw new IllegalStateException("Unable to find achievement");
+        }
+        
     }
 
     
